e81d4b491b4587b2c3c0e12c9c2dc8a54c71b566
Komputer-P <wlghks0314@gmail.com>
2022-03-31 17:05:29 +0900

    specified comment added

--- src/main/java/kom/komputer/springstudy/springboot/Application.java
+++ src/main/java/kom/komputer/springstudy/springboot/Application.java
@@ -3,9 +3,9 @@ package kom.komputer.springstudy.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication // 스프링 부트의 자동 설정, 스프링 Bean 읽기/생성 모두 자동 설정
+@SpringBootApplication // 스프링 부트의 자동 설정 -> classpath 를 참고하여 스프링 Bean 을 생성함 (ex: spring-webmvc 가 classpath 에 있으면 DispatcherServlet + 내장 WAS(Tomcat)을 자동 설정해줌)
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args); // 내장 WAS (Tomcat)을 실행함
+        SpringApplication.run(Application.class, args); // (웹) 애플리케이션(WAS (Tomcat))을 실행함 -> ApplicationContext 이 생성됨
     }
 }
